9c16af9adbc40eaba2952422faffaa02d9635bdb
nhatruong <nhat_truong@outlookcom>
2017-10-07 08:02:36 -0700

    Assignment 4

---
+++ Assignment4/src/fromNumberToRomanNumerals/Test.java
@@ -0,0 +1,12 @@
+package fromNumberToRomanNumerals;
+
+public class Test {
+
+	public static void main(String[] args) {
+		NumberToRomanNumeralsConversion r = new NumberToRomanNumeralsConversion();
+		System.out.println(r.intToRoman(16));
+		System.out.println(r.intToRoman(3456));
+		System.out.println(r.intToRoman(3999));
+	}
+
+}
